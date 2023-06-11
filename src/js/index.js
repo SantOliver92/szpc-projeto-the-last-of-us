37a76664de2e982e616971d077af5753f7edf005
nathsantoliver <nathaliaolisantana@gmail.com>
2023-06-10 23:06:40 -0300

    corrigindo erros do index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,38 @@
+(function() {
+    
+    'use strict';
 
-const botoesCarrossel = document.querySelectorAll('.botao');
-const imagens = document.querySelectorAll('.imagem');
+    const botoesCarrossel = document.querySelectorAll('.botao');
+    const imagens = document.querySelectorAll('.imagem');
+    
+    botoesCarrossel.forEach((botao, indice) => {
+        botao.addEventListener('click', () => {
+            desativarBotaoSelecionado();
+    
+            selecionarBotaoCarrossel(botao);
+    
+            esconderImagemAtiva();
+    
+            mostrarImagemDeFundo(indice);
+        });
+    });
+    
+    function mostrarImagemDeFundo(indice) {
+        imagens[indice].classList.add('ativa');
+    }
+    
+    function selecionarBotaoCarrossel(botao) {
+        botao.classList.add('selecionado');
+    }
+    
+    function esconderImagemAtiva() {
+        const imagemAtiva = document.querySelector('.ativa');
+        imagemAtiva.classList.remove('ativa');
+    }
+    
+    function desativarBotaoSelecionado() {
+        const botaoSelecionado = document.querySelector('.selecionado');
+        botaoSelecionado.classList.remove('selecionado');
+    }
 
-botoesCarrossel.forEach((botao, indice) => {
-    botao.addEventListener('click', () => {
-        desativarBotaoSelecionado();
-
-        selecionarBotaoCarrossel(botao);
-
-        esconderImagemAtiva();
-
-        mostrarImagemDeFundo(indice);
-    })
-})
-
-function mostrarImagemDeFundo(indice) {
-    imagens[indice].classList.add('ativa');
-}
-
-function selecionarBotaoCarrossel(botao) {
-    botao.classList.add('selecionado');
-}
-
-function esconderImagemAtiva() {
-    const imagemAtiva = document.querySelector('.ativa');
-    imagemAtiva.classList.remove('ativa');
-}
-
-function desativarBotaoSelecionado() {
-    const botaoSelecionado = document.querySelector('.selecionado');
-    botaoSelecionado.classList.remove('selecionado');
-}
+})();
